b817af8f5ae54eeb22bd22df4e4d0a55daf75403
Roman <dev.vrv@gmail.com>
2023-12-26 16:54:40 +0600

    core

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Main from '../Main/Main';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Main/Main.jsx
@@ -0,0 +1,11 @@
+import './Main.scss'
+
+function Main() {
+    return (
+        <main className="main">
+            
+        </main>
+    )
+}
+
+export default Main
